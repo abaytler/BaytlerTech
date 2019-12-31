4ca5d0d7985e6d94d2e2f2f459b099def4931172
Andrew Baytler <abaytler@gmail.com>
2019-12-30 19:16:47 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+//import * as serviceWorker from './serviceWorker';
+import { unregister } from '.serviceWorker';
+unregister();
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
